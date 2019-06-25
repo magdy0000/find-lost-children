2b118b2945282ca13e894376318fc8020d0aa70b
Youssef3ssam <youssef.3ssam1@gmail.com>
2019-06-25 19:19:13 +0200

    Add ToolBar

--- app/src/main/java/com/example/findlostchildren/Activities/MainActivity.java
+++ app/src/main/java/com/example/findlostchildren/Activities/MainActivity.java
@@ -7,9 +7,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
+import android.view.MenuItem;import android.view.View;
+import android.support.v7.widget.Toolbar;
 
 import com.example.findlostchildren.Fragments.HomeFragment;
 import com.example.findlostchildren.Fragments.NotificationFragment;
@@ -17,20 +16,25 @@ import com.example.findlostchildren.Fragments.PersonalVictimFragment;
 import com.example.findlostchildren.Fragments.ProfileFragment;
 import com.example.findlostchildren.Fragments.SearchFragment;
 import com.example.findlostchildren.R;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
     private FragmentManager fragmentManager;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         fragmentManager = getSupportFragmentManager();
-        loadStartFragment();
+        toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 
+        getSupportActionBar().setTitle("Home");
+        loadStartFragment();
 
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setItemIconTintList(null);
@@ -40,19 +44,23 @@ public class MainActivity extends AppCompatActivity {
                 switch (item.getItemId()) {
                     case R.id.action_home:
                         loadFragment(new HomeFragment());
+                        getSupportActionBar().setTitle("Home");
                         break;
                     case R.id.action_myVictims:
                         loadFragment(new PersonalVictimFragment());
-
+                        getSupportActionBar().setTitle("My Victims");
                         break;
                     case R.id.action_search:
                         loadFragment(new SearchFragment());
+                        getSupportActionBar().setTitle("Search");
                         break;
                     case R.id.action_profile:
                         loadFragment(new ProfileFragment());
+                        getSupportActionBar().setTitle("Profile");
                         break;
                     case R.id.action_notification:
                         loadFragment(new NotificationFragment());
+                        getSupportActionBar().setTitle("Notification");
                         break;
                 }
                 return true;
@@ -73,7 +81,6 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().popBackStack();
         fragmentTransaction.commit();
     }
-
 
 
 }
