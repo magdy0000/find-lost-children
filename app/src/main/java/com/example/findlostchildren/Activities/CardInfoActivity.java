9210cee4bef8381a8f1cc7683909671470357ca4
magdy0000 <magdy.mahmoud0000@outlook.com>
2019-06-27 05:20:37 +0200

    c

--- app/src/main/java/com/example/findlostchildren/Activities/CardInfoActivity.java
+++ app/src/main/java/com/example/findlostchildren/Activities/CardInfoActivity.java
@@ -1,21 +1,48 @@
 package com.example.findlostchildren.Activities;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.findlostchildren.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class CardInfoActivity extends AppCompatActivity {
 
-    Button known , unknown;
+
+
+   DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_info);
 
 
+        ref.child("Base64Images").orderByChild("victimId").equalTo("-LiLCXw-zvn0Jzs_N8SNv").equalTo("a").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Toast.makeText(CardInfoActivity.this, dataSnapshot.getValue()+"", Toast.LENGTH_SHORT).show();
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
 
 
     }
+
+
 }
